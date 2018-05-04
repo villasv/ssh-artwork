5484122fe6c8ac397f56db193ea27cb2e1c06f1d
Victor Villas <villasv@outlook.com>
2018-05-04 01:27:11 +0000

    Fix target causing misalignment

--- main.py
+++ main.py
@@ -13,10 +13,13 @@ def diff(a_art, b_art):
         if (a == ' ' and b in heavy_set) or (a in heavy_set and b == ' '):
             return 100
         if (a == ' ' and b in light_set) or (a in light_set and b == ' '):
-            return 25
+            return 50
         if (a in light_set and b in heavy_set) or (a in heavy_set and b in light_set):
             return 10
-        return 1
+        if (a in light_set and b in light_set) or (a in heavy_set and b in heavy_set):
+            return 1
+        
+        raise Exception("Should never happen! '{}'<>'{}'".format(a, b))
 
     from operator import add
     from functools import reduce
@@ -29,7 +32,7 @@ def search(_):
 
     import os
     _, _, art_files = next(os.walk('./keys'))
-    approx_file = './keys/' + sorted(art_files)[-1]
+    approx_file = './keys/' + sorted(art_files)[-2]
 
     with open(approx_file) as f:
         approx_art = f.read()
@@ -47,7 +50,7 @@ def search(_):
         approx_file = "./keys/{}.art".format(index+1)
         with open(approx_file, 'w') as f:
             approx_art = f.write(newest_art)
-        with open(approx_file[:-3] + '.key', 'w') as f:
+        with open(approx_file[:-3] + 'key', 'w') as f:
             approx_art = f.write(str(private_key, 'utf-8'))
     
 
@@ -56,5 +59,5 @@ from tqdm import tqdm
 
 if __name__ == "__main__":
     attempts = 1000
-    with Pool(processes=3) as p:
+    with Pool(processes=1) as p:
         r = list(tqdm(p.imap(search, range(attempts)), total=attempts))
